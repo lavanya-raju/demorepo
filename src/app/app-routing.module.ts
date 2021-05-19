ad99c3625c1008ec2085ceaa3c65768a8e7ffdd6
lavanya-raju <84458104+lavanya-raju@users.noreply.github.com>
2021-05-19 23:34:52 +0530

    Demo commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutComponent} from './about/about.component';
+import { AppComponent } from './app.component';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {AuthGuard} from './_guards/auth.guard';
+import {GalleryComponent} from './gallery/gallery.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path : 'about', component: AboutComponent},
+  {path : 'home', component: HomeComponent},
+  {path : 'login', component: LoginComponent},
+  {path : 'gallery', component: GalleryComponent, canActivate: [AuthGuard]},
+  {path : 'app', component: AppComponent},
+  {path : '**', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BackendProvider } from './_helpers/backend';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    AppHeaderComponent,
+    AppFooterComponent,
+    AboutComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BackendProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
